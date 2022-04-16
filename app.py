a71dea9e0f10e90b68e170f3cca1a5fbfae7466f
Beatriz Gonçalves <Beacg19@gmail.com>
2022-04-16 13:13:18 +0100

    update

--- app.py
+++ app.py
@@ -239,11 +239,8 @@ choose_tab = dcc.Tabs([
                     html.H5('Continent Choice', style={'textAlign': 'center', 'color': colors["text"]}),
                     dropdown_scope,
                     html.Br(),
-                    html.H5('Which Projection?', style={'textAlign': 'center', 'color': colors["text"]}),
-                    dbc.Row([
-                        html.P('Orthographic', style={'paddingLeft': '80px', 'paddingRight': '8px', 'color': '#fff'}),
-                        checklist_projection
-                    ])
+                    html.H5('Orthographic or Equirectangular?', style={'textAlign': 'center', 'color': colors["text"]}),
+                    checklist_projection
                 ], style={'width': '30%'}, className='slicerblue'),
             
                 html.Div([
